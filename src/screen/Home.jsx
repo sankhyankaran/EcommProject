f66dfe6ac68ddea0b337f2f86f55cec4c86cb758
sankhyankaran <karansrma2003@gmail.com>
2024-02-08 22:51:54 +0530

    Add Category

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 function Home() {
   const [data, setData] = useState([]);
   const [product, setProducts] = useState([]);
-
+  const [category, setCategory] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,12 +12,12 @@ function Home() {
       .then((res) => {
         setData(res.data.products);
         setProducts(res.data.products);
-        console.log(res.data.products, "res data ===");
+         console.log(res.data.products, "res data ===");
       })
       .catch((err) => {
         console.log(err);
       });
-  },[]);
+  }, []);
 
   const Product = (d) => {
     setProducts(
@@ -28,6 +28,9 @@ function Home() {
       )
     );
   };
+  const Category = (e) => {
+    setProducts(data.filter((f) => f.category.includes(e.target.value)));
+  };
 
   return (
     <>
@@ -35,7 +38,19 @@ function Home() {
       <div>
         <input type="text" placeholder="Search" onChange={Product}></input>
       </div>
-      
+      <div className="select-product">
+        <label htmlFor="category" className="m-2">
+          Category :
+        </label>
+        <select id="category" name="Filter" onChange={Category}>
+          <option></option>
+          <option value=""> All Product</option>
+          <option value="smartphones">Phone</option>
+          <option value="fragrances">Perfume</option>
+          <option value="skincare">Others</option>
+        </select>
+      </div>
+
       <span id="Images">
         {product.map((d, index) => (
           <span key={index}>
@@ -49,6 +64,23 @@ function Home() {
             <p>{d.title}</p>
             <p>{d.price}</p>
             <p>{d.category}</p>
+          </span>
+        ))}
+      </span>
+
+      {/* Category map array */}
+      <span id="images">
+        {category.map((item, index) => (
+          <span key={index}>
+            <img
+              src={item.thumbnail}
+              alt="Product image"
+              height="20%"
+              width="20%"
+            />
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+            <p>{item.category}</p>
           </span>
         ))}
       </span>
--- src/screen/Register.jsx
+++ src/screen/Register.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React from "react";
 
 function Register() {
   return (
     <>
+      <div className="card text-center mx-auto">
+        <div className="card-header bg-info text-white">New User</div>
+        <div className="card-body">
+          <div className="form-group row">
+            <label className="col-sm-4" for="txtfirstname">
+              First Name
+            </label>
+            <div className="col-sm-6">
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter first Name"
+                className="form-control"
+                id="txtfirstname"
+              />
+            </div>
+          </div>
+          <div className="form-group row m-2">
+            <label className="col-sm-4" for="txtlastname">
+              Last Name
+            </label>
+            <div className="col-sm-6">
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter last Name"
+                className="form-control"
+                id="txtlastname"
+              />
+            </div>
+          </div>
+          <div className="form-group row m-2">
+            <label className="col-sm-4" for="txtemail">
+              Email
+            </label>
+            <div className="col-sm-6">
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter email"
+                className="form-control"
+                id="txtemail"
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-4" for="txtpassword">
+              Password
+            </label>
+            <div className="col-sm-6">
+              <input
+                type="text"
+                name="password"
+                placeholder="Enter password"
+                className="form-control"
+                id="txtpassword"
+              />
+            </div>
+          </div>
+          <div className="form-group row m-2">
+            <label className="col-sm-4" for="txtconfirmPassword">
+              Confirm Password
+            </label>
+            <div className="col-sm-6">
+              <input
+                type="text"
+                name="confirmPassword"
+                placeholder="Confirm password"
+                className="form-control"
+                id="txtconfirmPassword"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted">
+          <button className="btn btn-info">Register</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
